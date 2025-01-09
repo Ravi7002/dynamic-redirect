5a70f2e7b07e20c33e3ae63c5c8969db32d78b17
Ravi7002 <160730256+Ravi7002@users.noreply.github.com>
2025-01-10 06:44:39 +0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -4,11 +4,9 @@ const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 if (/android/i.test(userAgent)) {
   // Redirect to the Play Store for Android users
   window.location.href = "https://play.google.com/store/apps/details?id=com.figma.mirror";
-  window.location.href = "https://embed.figma.com/proto/lPOfctNuCvyMTKD2MaszOG/App-Mockup?node-id=1-7&p=f&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A7&show-proto-sidebar=1&embed-host=share";
 } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
   // Redirect to the App Store for iOS users
   window.location.href = "https://itunes.apple.com/app/figma-mirror/id1152747299";
-  window.location.href = "https://embed.figma.com/proto/lPOfctNuCvyMTKD2MaszOG/App-Mockup?node-id=1-7&p=f&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A7&show-proto-sidebar=1&embed-host=share";
 } else {
   // Redirect to the Figma project for desktop users
   window.location.href = "https://embed.figma.com/proto/lPOfctNuCvyMTKD2MaszOG/App-Mockup?node-id=1-7&p=f&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A7&show-proto-sidebar=1&embed-host=share";
